73b0bd60794cb71e68adc7e887a4700ddf01862b
louis <louis.lamonte@moringaschool.com>
2024-05-04 14:31:51 +0300

    initial commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,62 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-level heading with the text `Hi, I'm _______`", () => {
+    render(<App />);
+  
+    const topLevelHeading = screen.getByRole("heading", {
+      name: /hi, i'm/i,
+      exact: false,
+      level: 1,
+    });
+  
+    expect(topLevelHeading).toBeInTheDocument();
+  });
+  
+  test("displays an image of yourself", () => {
+    render(<App />);
+  
+    const image = screen.getByAltText("My profile pic");
+  
+    expect(image).toHaveAttribute("src", "https://via.placeholder.com/350");
+  });
+  
+  test("displays second-level heading with the text `About Me`", () => {
+    render(<App />);
+  
+    const secondLevelHeading = screen.getByRole("heading", {
+      name: /about me/i,
+      level: 2,
+    });
+  
+    expect(secondLevelHeading).toBeInTheDocument();
+  });
+  
+  test("displays a paragraph for your biography", () => {
+    render(<App />);
+  
+    const bio = screen.getByText(/lorem ipsum/i);
+  
+    expect(bio).toBeInTheDocument();
+  });
+  
+  test("displays the correct links", () => {
+    render(<App />);
+  
+    const githubLink = screen.getByRole("link", {
+      name: /github/i,
+    });
+    const linkedinLink = screen.getByRole("link", {
+      name: /linkedin/i,
+    });
+  
+    expect(githubLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://github.com")
+    );
+  
+    expect(linkedinLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://linkedin.com")
+    );
+  });
